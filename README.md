f56593ab76547d62e9505f609d21da6cb0cfcf9d
Justin Roll <justin.j.roll@gmail.com>
2017-04-30 12:11:27 -0700

    Initial working version of the 37th map.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# JustinRoll.github.io
+# Demo site for the 37th PCO Map
+This was forked from the 34th PCO map here:
+https://github.com/corespace/34DemsPCOMap
---
+++ map.js
@@ -0,0 +1,109 @@
+var globals;
+console.log("found js file");
+var Globals = class {
+  constructor(map, geocoder, infoWindow) {
+    this.map = map;
+    this.geocoder = geocoder;
+    this.infoWindow = infoWindow;
+  }
+
+  GetMap() {
+    return this.map;
+  }
+
+  GetGeocoder() {
+    return this.geocoder;
+  }
+
+  GetInfoWindow() {
+    return this.infoWindow;
+  }
+}
+
+
+var InitMap = function() {
+  var initPoint = {lat: 47.60, lng: -122.36}
+
+  var geocoder = new google.maps.Geocoder();
+  var infoWindow = new google.maps.InfoWindow()
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: initPoint,
+    zoom: 11
+  });
+
+  google.maps.event.addListener(map,'click',function() {
+        globals.GetInfoWindow().close();
+  });
+
+  globals = new Globals(map, geocoder, infoWindow);
+  DrawAllPrecincts();
+}
+
+var GeoCodeAddress = function() {
+  var address = document.getElementById('address-box').value;
+  globals.GetGeocoder().geocode( { 'address': address}, function(results, status) {
+    if (status == 'OK') {
+      globals.GetMap().setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: globals.GetMap(),
+          position: results[0].geometry.location
+      });
+      globals.GetInfoWindow().close();
+      //CreateInfoWindow();
+      //globals.GetInfoWindow().open(globals.GetMap(), marker);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+var DrawAllPrecincts = function() {
+  var url = "all_precincts.json";
+
+  globals.GetMap().data.loadGeoJson(url);
+  
+  //Set any precinct that has no PCO to red
+  globals.GetMap().data.setStyle(function(feature) {
+    var pco = feature.getProperty('pco');
+    var color = pco ? 'white' : '#dd9933';
+    return {
+      fillColor: color,
+    };
+  });
+
+console.log(globals.GetMap())
+  //if user clicks, show PCO info
+  globals.GetMap().data.addListener('click', function(event){
+    CreateInfoWindow(event.feature);
+    var anchor = new google.maps.MVCObject();
+    anchor.set("position", event.latLng)
+    globals.GetInfoWindow().open(globals.GetMap(), anchor);
+  });
+}
+
+
+var CreateInfoWindow = function(feature) {
+    var pcoContent = '<div id="content">'
+      + '<h3>Precinct: ' + feature.getProperty('name') + '</h3>';
+    var pcoInfo = feature.getProperty('pco');
+
+    if(pcoInfo) {
+      pcoContent = pcoContent  +  
+      '<h5>' + pcoInfo['first'] + ' ' + pcoInfo['last'] + '</h5>';
+    } else {
+      pcoContent = pcoContent + '<p> No PCO currently.</p>' + 
+        '<a href="http://37dems.org/index.php?page=display&id=130"> Click here to be appointed!</a>';
+    }
+
+    pcoContent = pcoContent + '</div>';
+
+    //Set Info Window
+    globals.GetInfoWindow().setContent(pcoContent);
+}
+
+function precincts_callback(response) {
+  globals.GetMap().data.addGeoJson(response);
+}
+
+
+
